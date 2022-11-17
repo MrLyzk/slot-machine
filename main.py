257583025eb864f6f004160ab7a6e2ca42461274
jakub <kkrusicki@gmail.com>
2022-11-17 15:26:40 +0100

    print_slot_machine function successfully added

--- main.py
+++ main.py
@@ -21,18 +21,29 @@ def get_slot_machine_spin(rows, cols, symbols):
         for _ in range(sym_count):
             all_symbols.append(symbol)
 
-    columns = [[], [], []]
+    columns = []
     for _ in range(cols):
         column = []
         current_symbols = all_symbols[:]
         for _ in range(rows):
-            value = random.choice(all_symbols)
+            value = random.choice(current_symbols)
             current_symbols.remove(value)
-            column.append()
+            column.append(value)
 
         columns.append(column)
 
     return columns
+
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+
+        print()
 
 
 def deposit():
@@ -91,6 +102,9 @@ def main():
         else:
             print("Insufficient balance ($" + str(balance) + "), please bet lower amount.")
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to ${total}")
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 
 main()
